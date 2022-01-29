e0168a217b0d250747869a88de73f89e5938bfac
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-01-29 09:55:54 -0300

    Adicionando visualização de score

---
+++ app.js
@@ -0,0 +1,48 @@
+const quizContainer = document.querySelector('.quiz')
+const form = document.querySelector('.quiz-form')
+const correctAnswers = ['B', 'B', 'B', 'A', 'B']
+
+const showScore = score => {
+
+    document.querySelector('.feedbackDiv') 
+    ? document.querySelector('.feedbackDiv').remove()
+    : ''
+    
+    const scoreDiv = document.createElement('div')
+
+    const colorClass = score >= 60 
+    ? 'bg-success'
+    : 'bg-danger'
+
+    const feedbackMessage = score >= 60 
+    ? `Score: ${score} | Eis aqui um verdadeiro amante do lolzinho. Parabéns =)`
+    : `Score: ${score} | Quem sabe na próxima... Vá praticar nos campos da justiça! =(`
+
+    scoreDiv.classList.add('feedbackDiv', 'py-2', 'text-center',
+     'text-white', 'lead', 'font-weight-normal', colorClass)
+    scoreDiv.textContent = feedbackMessage
+
+    quizContainer.prepend(scoreDiv)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    let score = 0
+
+    const userAnswers = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value,
+        form.inputQuestion5.value,
+    ]
+
+    userAnswers.forEach((userAnswer, index) => {
+        userAnswer === correctAnswers[index] 
+        ? score += 20
+        : ''
+    })
+    
+    showScore(score)
+})
